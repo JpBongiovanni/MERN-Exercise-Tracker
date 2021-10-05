485d21b0cf48cd26adcc6d5a92140275dd8e1a55
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-10-05 14:25:53 -0500

    stopped at 15:10

--- src/components/stopWatch.component.js
+++ src/components/stopWatch.component.js
@@ -5,10 +5,15 @@ import './stopWatch.component.css';
 
 function StopWatch() {
     const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+    const [interv, setInterv] = useState();
+    const [status, setStatus] = useState(0);
+    // Not started = 0;
+    // started = 1;
+    // stopped = 2; 
 
     const start = () => {
         run();
-        setInterval(run, 10);
+        setInterv(setInterval(run, 10));
     }
 
     var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
@@ -29,13 +34,19 @@ function StopWatch() {
         updatedMs++;
         return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH})
 
+    };
+
+    const stop = () => {
+        clearInterval(interv);
+        setStatus(2)
     }
+
     return (
         <div className="main-section">
             <div className="clock-holder">
                 <div className="stopwatch">
                     <DisplayComponent time={time}/>
-                    <BtnComponent start={start}/>
+                    <BtnComponent status={status} start={start}/>
                 </div>
             </div>
         </div>
